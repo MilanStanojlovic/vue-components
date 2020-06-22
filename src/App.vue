1183e6630f7ee99971415dea63704ea700c0d499
Milan <mistikone1@gmail.com>
2020-06-22 15:26:01 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container">
+    <app-header></app-header>
+    <hr />
+    <app-servers></app-servers>
+    <hr />
+    <app-footer></app-footer>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style></style>
---
+++ src/components/ServerList.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="row">
+    <div class="col-xs-12 col-sm-6">
+      <ul class="list-group">
+        <app-server></app-server>
+      </ul>
+    </div>
+    <div class="col-xs-12 col-sm-6">
+      <p>Server Details are currently not updated</p>
+    </div>
+  </div>
+</template>
+<script>
+import Server from "./Server.vue";
+export default {
+  components: {
+    "app-server": Server
+  }
+};
+</script>
